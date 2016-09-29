a126c686111343a645953c6d604a628790471333
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-09-29 18:41:10 -0300

    server: adding support to heroku

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,26 @@ extern crate iron;
 extern crate mount;
 extern crate staticfile;
 
-use std::io;
-use std::fs;
-
-use iron::prelude::*;
-use iron::status;
-use iron::method;
-use mount::Mount;
-use staticfile::Static;
+use std::env;
 use std::path::Path;
 
+use std::str::FromStr;
+use iron::prelude::*;
+use mount::Mount;
+use staticfile::Static;
+
+
+/// Look up our server port number in PORT, for compatibility with Heroku.
+fn server_port() -> u16 {
+    let port_str = env::var("PORT").unwrap_or(String::new());
+    FromStr::from_str(&port_str).unwrap_or(3000)
+}
 
 fn main() {
     let mut mount = Mount::new();
     // Serve the shared JS/CSS at /
     mount.mount("/", Static::new(Path::new("static/")));
-    println!("Running on: http://localhost:3000");
-    Iron::new(mount).http("127.0.0.1:3000").unwrap();
+    let port = server_port();
+    println!("Running on: http://localhost:{}", port);
+    Iron::new(mount).http(("0.0.0.0", port)).unwrap();
 }
